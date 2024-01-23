71c0189d6f73145800f7d948f94c8401ebd603d1
Jacob Padilla <jp@jacobpadilla.com>
2024-01-23 00:14:38 -0500

    some more aoefwnf-paw

--- gdtch/mixins/alter_text.py
+++ gdtch/mixins/alter_text.py
@@ -1,13 +1,13 @@
 import re
-from lxml import html
+from lxml import html, etree
 
 class AlterText:
     def insert_inline_code(self) -> None:
-        pattern = r'`(.*?)`'
+        pattern = re.compile(r'`(.*?)`')
 
-        for element in self.elements:
+        for i, element in enumerate(self.elements):
             if element.text:
-                text = element.text
-                text = re.sub(pattern, r'<code class="inline-code">\1</code>', text)
-                element.clear()
-                element.append(html.fromstring(text))
+                print('here')
+                text = html.tostring(element, method='html', encoding='utf-8').decode('utf-8')
+                text = pattern.sub(r'<code class="inline-code">\1</code>', text)
+                self.elements[i] = html.fromstring(text)
--- gdtch/mixins/remove_junk_tags.py
+++ gdtch/mixins/remove_junk_tags.py
@@ -9,21 +9,20 @@ class RemoveJunkTags:
         while i < len(self.elements):
             tag = self.elements[i]
 
-            for span in tag.iterfind('span'):
-                span.drop_tag()
-
             if len(tag.xpath('.//text()')) == 0 or tag.xpath('.//text()')[0].isspace():
                 self.elements.pop(i)
 
             else:
+                for span in tag.iterfind('span'):
+                    span.drop_tag()
+
                 html_string = html.tostring(tag, encoding='utf-8', method='html').decode('utf-8')
 
-                # html_string = re.sub(r'<span[^>]*>|</span>', '', html_string)
                 html_string = re.sub(r'\s+</p>', '</p>', html_string)
-                html_string = re.sub(r'(?<!\w)"', '“', html_string)  # Opening double quotes
-                html_string = re.sub(r'"', '”', html_string)  # Closing double quotes
-                html_string = re.sub(r"(?<!\w)'", '‘', html_string)  # Opening single quotes
-                html_string = re.sub(r"'", '’', html_string)  # Closing single quotes
+                html_string = re.sub(r'(?<!\w)"', '“', html_string) 
+                html_string = re.sub(r'"', '”', html_string) 
+                html_string = re.sub(r"(?<!\w)'", '‘', html_string)
+                html_string = re.sub(r"'", '’', html_string)
 
                 self.elements[i] = html.fromstring(html_string)
 
