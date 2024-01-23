d05cf1dacaea3cf2d5c1542d368e827228765415
Jacob Padilla <jp@jacobpadilla.com>
2024-01-22 22:49:06 -0500

    some more small stuff

--- gdtch/mixins/add_attributes.py
+++ gdtch/mixins/add_attributes.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlparse
-
+import re
 
 class AddAttributes:
     def add_target_to_outgoing_links(self, /, origin: str, target: str) -> None:
@@ -8,5 +8,7 @@ class AddAttributes:
                 if urlparse(a.attrib['href']).hostname != origin:
                     a.attrib['target'] = target
 
-    def generate_header_id_attributes(self):
-        pass
+    def generate_header_id_attributes(self) -> None:
+        for element in self.elements:
+            if re.match(r'h[1-6]$', element.tag):
+                element.attrib['id'] = '-'.join(element.text.lower().split())
