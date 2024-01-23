352ce4904d3ddb85ef5baefbf1a67773200197cc
Jacob Padilla <jp@jacobpadilla.com>
2024-01-22 23:46:21 -0500

    went back to span drop tag

--- gdtch/mixins/remove_junk_tags.py
+++ gdtch/mixins/remove_junk_tags.py
@@ -9,13 +9,16 @@ class RemoveJunkTags:
         while i < len(self.elements):
             tag = self.elements[i]
 
+            for span in tag.iterfind('span'):
+                span.drop_tag()
+
             if len(tag.xpath('.//text()')) == 0 or tag.xpath('.//text()')[0].isspace():
                 self.elements.pop(i)
 
             else:
                 html_string = html.tostring(tag, encoding='utf-8', method='html').decode('utf-8')
 
-                html_string = re.sub(r'<span[^>]*>|</span>', '', html_string)
+                # html_string = re.sub(r'<span[^>]*>|</span>', '', html_string)
                 html_string = re.sub(r'\s+</p>', '</p>', html_string)
                 html_string = re.sub(r'(?<!\w)"', '“', html_string)  # Opening double quotes
                 html_string = re.sub(r'"', '”', html_string)  # Closing double quotes
