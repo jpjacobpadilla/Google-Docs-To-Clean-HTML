ad8a7d140d0f6a3a98e3b92e91a6635aced7dd07
Jacob Padilla <jp@jacobpadilla.com>
2024-01-22 21:22:35 -0500

    refactored this one

--- gdtch/mixins/remove_top_of_document.py
+++ gdtch/mixins/remove_top_of_document.py
@@ -1,6 +1,6 @@
 class RemoveTopOfDocument:
     def remove_top_of_document(self, element_break: str = 'hr') -> None:
-        for item in self.root.iterfind('./body/*'):
-            item.drop_tree()
+        for item in self.elements:
             if item.tag == element_break:
                 break
+            item.drop_tree()
