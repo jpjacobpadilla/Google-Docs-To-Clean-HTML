c02197fdad4c584132ceca173e500fa6e41345da
Jacob Padilla <jp@jacobpadilla.com>
2024-01-23 18:46:03 -0500

    added the NoBreakElementFound exception

--- gdtch/mixins/remove_top_of_document.py
+++ gdtch/mixins/remove_top_of_document.py
@@ -1,10 +1,12 @@
 from gdtch.exceptions import NoBreakElementFound
 
+
 class RemoveTopOfDocument:
     def remove_top_of_document(self, element_break: str = 'hr') -> None:
-        raise NoBreakElementFound()
-        while self.elements[0].tag != element_break:
+        while len(self.elements) > 0 and self.elements[0].tag != element_break:
             self.elements.pop(0)
 
-        if self.elements[0].tag == element_break:
-            self.elements.pop(0)
+        if len(self.elements) == 0:
+            raise NoBreakElementFound()
+
+        self.elements.pop(0)
