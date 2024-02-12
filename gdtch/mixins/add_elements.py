a3127310b60badebcce2f0814c87e7287e1943f6
Jacob Padilla <jp@jacobpadilla.com>
2024-02-12 15:38:08 -0500

    wrap li content in a p tag

--- gdtch/mixins/add_elements.py
+++ gdtch/mixins/add_elements.py
@@ -11,3 +11,35 @@ class AddElements:
                 i += 1
 
             i += 1
+    
+    def wrap_li_content_in_p_tag(self) -> None:
+        """
+        Transform:
+
+            <ul>
+                <li>content</li>
+            </ul>
+
+        To:
+        
+            <ul>
+                <li><p>content</p></li>
+            </ul>
+        """
+        for element in self.elements:
+            if element.tag == 'ul':
+                for li in element:
+                    # Create a new <p> element
+                    p_content = html.Element('p')
+
+                    # Move text (if any) of the <li> element to the <p> element
+                    if li.text:
+                        p_content.text = li.text.strip()
+                        li.text = None  # Clear the text from the <li> after moving it
+
+                    # Move all children of the <li> element to the <p> element
+                    for child in list(li):
+                        p_content.append(child)
+                        li.remove(child)
+
+                    li.append(p_content)
