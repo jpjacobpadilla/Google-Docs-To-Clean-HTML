63de26ec3d8d107c076c7ad3736ab561d7a2a3b3
Jacob Padilla <jp@jacobpadilla.com>
2025-05-29 22:42:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ setup(
     version='1.0',
     packages=find_packages(),
     install_requires=['lxml'],
-    author='Jacob Padilla',
-    author_email='jp@jacobpadilla.com'
+    author='Jacob Padilla'
 )
