bf64ed97b6f7108479f2198736a6a3cd85e23754
Jacob Padilla <jp@jacobpadilla.com>
2025-06-17 17:35:41 -0400

    UV

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='gdtch',
-    version='1.0',
-    packages=find_packages(),
-    install_requires=['lxml'],
-    author='Jacob Padilla'
-)
